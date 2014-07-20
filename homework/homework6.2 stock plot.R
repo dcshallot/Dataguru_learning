f48be527f23106e80a5167e16093de79e5d431b5
dcshallot <dcshallot@yahoo.com>
2014-07-20 16:42:03 +0800

    2

--- homework/homework6.2 stock plot.R
+++ homework/homework6.2 stock plot.R
@@ -10,13 +10,17 @@ aapl<- get.hist.quote(instrument = "aapl", quote =  "Vol", start="2008-01-01", e
 goog <- get.hist.quote(instrument = "goog", quote = "Vol", start="2008-01-01", end="2011-12-31")
 msft <- get.hist.quote(instrument = "msft", quote = "Vol", start="2008-01-01", end="2011-12-31")
 
+cyou <- get.hist.quote(instrument = "cyou", quote = "Vol", start="2008-01-01", end="2011-12-31")
+
+??get.hist.quote
+
 ap <- as.numeric( Vo( to.yearly(aapl)))
 go <- as.numeric( Vo( to.yearly(goog)))
 ms <- as.numeric( Vo( to.yearly(msft)))
 
 stock <- data.frame(yr=c("2008" , "2009" , "2010", "2011") ,  
                     apple = ap/sum(ap), 
-                    google = go/sum(go), 
+                    #google = go/sum(go), 
                     microsoft = ms/sum(ms))
 
 
@@ -24,3 +28,4 @@ x <- barplot( as.matrix( stock[,-1]) , beside= T, col = rainbow(4), ylim=c(0,0.5
 y <- c(  ap/sum(ap), go/sum(go),  ms/sum(ms) )/2
 text( x, y , paste( round(y *100), "%", sep="" ), cex = 0.6 )
 legend( "topright", legend=stock$yr, horiz=T, bty="n" , cex = 0.8, pch=5, col= rainbow(4) )
+
